669a5df875cf27e5430730a7484a234c688fee25
TzachiBakshi <zachiba1@gmail.com>
2023-09-11 16:32:26 +0300

    main.py update

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from df_build import *
 from api import *
 
-new_row = {'Date': current_date, 'Followers': followers_count}
-
-df = df.append(new_row, ignore_index=True)
-
+for row in df:
+    if current_date in df['Date']:
+        continue
+    else:
+        new_row = {'Date': current_date, 'Followers': followers_count}
+        df.append(new_row, ignore_index=True)
+        
 print(df)
