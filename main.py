166ce8a8f0d6f12d8cf60cd61817cb192325db97
TzachiBakshi <zachiba1@gmail.com>
2023-09-11 16:39:13 +0300

    Optimise the "main.py" file

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from df_build import *
 from api import *
-
-for row in df:
-    if current_date in df['Date']:
-        continue
-    else:
-        new_row = {'Date': current_date, 'Followers': followers_count}
-        df.append(new_row, ignore_index=True)
+       
+# Check if 'current_date' already exists in the 'Date' column
+if current_date not in df['Date'].values:
+    new_row = {'Date': current_date, 'Followers': followers_count} # Create a new row as a dictionary
+    df = df.append(new_row, ignore_index=True) # Append the new row to the DataFrame
         
 print(df)
